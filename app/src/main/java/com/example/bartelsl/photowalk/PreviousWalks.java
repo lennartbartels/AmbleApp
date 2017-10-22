f5a2a8f8dafd5064fae4df515d4c5f8f435a4422
Nicolas Meyer <nicolasm@hpe.com>
2017-10-22 22:38:01 +0200

    Design - Update Seiten MyWalks und PreviousWalks
    ...jetzt können die Seiten aufgerufen werden...

--- app/src/main/java/com/example/bartelsl/photowalk/PreviousWalks.java
+++ app/src/main/java/com/example/bartelsl/photowalk/PreviousWalks.java
@@ -1,9 +1,10 @@
 package com.example.bartelsl.photowalk;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class PreviousWalks extends AppCompatActivity {
+public class PreviousWalks extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
